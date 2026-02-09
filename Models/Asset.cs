26569d495e5e05ee1ab59f9ab5ef8495fb1ca0dc
Asset Tracking Developer <cemcambell7278282@gmail.com>
2026-02-10 00:45:06 +0500

    Added core domain model classes

---
+++ Models/Asset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem.Models
+{
+    public class Asset
+    {
+        public int AssetId { get; set; }
+        public string DeviceName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string DeviceType { get; set; }
+        public string IPAddress { get; set; }
+        public string Notes { get; set; }
+
+        public int EmployeeId { get; set; }
+    }
+}
+
