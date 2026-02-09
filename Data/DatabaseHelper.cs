bed6c052111fe4b6db2a049b95130a3fda836f76
Asset Tracking Developer <cemcambell7278282@gmail.com>
2026-02-10 01:14:16 +0500

    Added InsertAsset method for SQLite database

--- Data/DatabaseHelper.cs
+++ Data/DatabaseHelper.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
-using System.IO;
+using AssetTrackingSystem.Models;
 
 namespace AssetTrackingSystem.Data
 {
     public static class DatabaseHelper
     {
-        private static string dbPath = "Data\\assets.db";
-        private static string connectionString = $"Data Source={dbPath};Version=3;";
+        private static string connectionString = "Data Source=assets.db;Version=3;";
 
-        public static SQLiteConnection GetConnection()
+        public static void InsertAsset(Asset asset)
         {
-            return new SQLiteConnection(connectionString);
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @"INSERT INTO Assets 
+                                (AssetId, DeviceName, Model, DeviceType, Manufacturer, IpAddress, Notes)
+                                VALUES 
+                                (@AssetId, @DeviceName, @Model, @DeviceType, @Manufacturer, @IpAddress, @Notes)";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@AssetId", asset.AssetId);
+                    command.Parameters.AddWithValue("@DeviceName", asset.DeviceName);
+                    command.Parameters.AddWithValue("@Model", asset.Model);
+                    command.Parameters.AddWithValue("@DeviceType", asset.DeviceType);
+                    command.Parameters.AddWithValue("@Manufacturer", asset.Manufacturer);
+                    command.Parameters.AddWithValue("@IpAddress", asset.IPAddress);
+                    command.Parameters.AddWithValue("@Notes", asset.Notes);
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
