90b806008aa7b4cf7c1ab6fb90e481b2ec2330c6
Asset Tracking Developer <cemcambell7278282@gmail.com>
2026-02-10 00:40:12 +0500

    Designed Add Asset form UI

--- AddAssetForm.cs
+++ AddAssetForm.cs
@@ -1,8 +1,8 @@
 namespace AssetTrackingSystem
 {
-    public partial class Form1 : Form
+    public partial class AddAssetForm : Form
     {
-        public Form1()
+        public AddAssetForm()
         {
             InitializeComponent();
         }
