001a647519c572c41967ae6581a59dc87bc1b956
Asset Tracking Developer <cemcambell7278282@gmail.com>
2026-02-10 00:59:56 +0500

    Added SQLite database setup and helper class

---
+++ DatabaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace AssetTrackingSystem
+{
+    public static class DatabaseHelper
+    {
+        private static string dbPath = "Data\\assets.db";
+        private static string connectionString = $"Data Source={dbPath};Version=3;";
+
+        public static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
+
